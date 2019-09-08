07271de8f9a25759fda4a662926272ef40b97a6a
0xbde <firstname@lastname.net>
2019-09-08 18:47:10 +0200

    step2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // Get express framework
 const express = require('express');
+const pug = require('pug');
 
 // Create an express application
 const app = express();
@@ -15,16 +16,15 @@ app.get('/', (req, res) => {
   }
     
   // Define template
-  let htmlContent = `
-    <h1>hello ${name}</h1></br>
-    <form action="/">
-      What is your name ?
-      <input type="text" name="name">
-      <input type="submit" value="Submit">
-    </form>`;
+  let template = `
+h1 hello ${name}
+form(method='GET' action='/')
+  label(for='name') What is your name ?
+  input#name.form-control(type='text', placeholder='first and last' name='name')
+  button.btn.btn-primary(type='submit') Submit`;
   
   // Render View
-  res.send(htmlContent);  
+  res.send(pug.render(template));  
 })
 
 
